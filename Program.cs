caa75b2127d192021bf27e62d9d27cc7afce539b
mloeff <michaelloeffler23@gmail.com>
2024-05-11 11:20:58 -0400

    creates two new methods to get and print employee names and calls them within the Main method

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static List<string> GetEmployees()
+    {
+      List<string> employees = new List<string>();
+      // Collect user values until the value is an empty string
+      while (true)
+      {
+        Console.WriteLine("Please enter a name: (leave empty to exit): ");
+        string input = Console.ReadLine() ?? "";
+        // Break if the user hits ENTER without typing a name
+        if (input == "")
+        {
+          break;
+        }
+        employees.Add(input);
+      }
+      return employees;
+    }
+
+    static void PrintEmployees(List<string> employees)
+    {
+      for (int i = 0; i < employees.Count; i++)
+      {
+        Console.WriteLine(employees[i]);
+      }
+    }
+    static void Main(string[] args) //Entry Point
+    {
+      // This is our employee-getting code now
+      List<string> employees = GetEmployees();
+      PrintEmployees(employees);
+    }
+
+  }
+}
