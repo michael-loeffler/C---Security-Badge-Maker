b24c356f869942808d2e6a9e9ba6e45b9085a6ec
mloeff <michaelloeffler23@gmail.com>
2024-05-12 20:48:25 -0400

    improves MakeBadges() method to create unique file names using employee ID numbers to prevent overwriting and to allow for the creation of multiple badges

--- obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1447a08b298cd06c6242f125edd3177c11871ed9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a89f000534f19f48ba6fbe4bc5cb7ca20c20a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
